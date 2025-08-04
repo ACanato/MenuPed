265252a1bde8565d1f30f3516cbdf33c08dce938
Canato <67785569+ACanato@users.noreply.github.com>
2025-08-04 13:07:02 +0100

    fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,14 +2,25 @@ fx_version 'bodacious'
 game 'gta5'
 
 author 'Canato'
-description 'Sistema de Ped'
-version '1.0.0'
+description 'System Ped'
+version '1.1.0'
 
 client_scripts {
+    'config.lua',
+    '@es_extended/locale.lua',
+    'locales/en.lua',
+    'locales/pt.lua',
+    'locales/es.lua',
+    'locales/fr.lua',
     'client.lua'
 }
 
 server_scripts {
+    'config.lua',
     '@es_extended/locale.lua',
+    'locales/en.lua',
+    'locales/pt.lua',
+    'locales/es.lua',
+    'locales/fr.lua',
     'server.lua'
 }
